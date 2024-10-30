a7062393951328a7081c31abd54812b0470cfeaf
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-10-30 11:18:15 +0100

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -118,7 +118,7 @@ export default defineConfig({
     ['meta', { property: 'og:type', content: 'website' }],
     // ['meta', { property: 'og:title', content: 'MikeCZ - Češtiny' }],
     // ['meta', { property: 'og:description', content: 'Stránka fanouškovského překladu hry.' }],
-    ['meta', { property: 'og:image', content: 'https://mikecz23.github.io/icon.png' }],  // Obrázek v odkazu
+    // ['meta', { property: 'og:image', content: 'https://mikecz23.github.io/icon.png' }],  // Obrázek v odkazu
     ['meta', { property:'theme-color', name: 'theme-color', content: '#ff6767' }], // barva odkazu
     ['meta', { property:'twitter:card', name: 'twitter:card', content: 'summary_large_image' }],
     ['meta', { name: 'google-site-verification', content: 'DmFx_aqenbpJTLNw4Pl_iK2Au3N_lEnhdDz0xreDVwA' }],
