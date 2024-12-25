d15cda8be1a32c4f3c0cc23566f9c45712c5c5b2
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-12-25 17:23:28 +0100

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -172,7 +172,7 @@ export default defineConfig({
           { text: 'Progressbar95', link: '/readme/progressbar95', activeMatch: "/progressbar95/"},
           { text: 'Infection Free Zone', link: '/readme/Infection', activeMatch: "/Infection/"}]},
       { text: '🛠️ Software', items: [
-        	{ text: "‎WIP", link: " ", activeMatch: " "}]},
+        	{ text: "‎XUnity.StringProcessor (WIP)", link: " ", activeMatch: " "}]},
  //       { text: 'Pertim', items: [
  //         { text: "Summer of 58", link: "/readme/summer", activeMatch: "/summer/"},
  //         { text: "Silver Chains", link: "/readme/silcha", activeMatch: "/silcha/" },
