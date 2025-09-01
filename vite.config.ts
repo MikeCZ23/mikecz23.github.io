1ff0c798ba1e3e286c9e6fe68657d7429d76d75f
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2025-09-01 13:05:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import path from 'path'   // <- potřeba pro path.resolve
 
 export default defineConfig({
   plugins:[
@@ -16,10 +15,5 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './.vitepress/theme')
-    }
-  }
+  ]
 })
